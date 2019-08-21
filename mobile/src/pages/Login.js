5a88267816bdfd9ec213f10ce72f42e3e48b7853
eltonlazzarin <elton.lazzarin@outlook.com>
2019-08-21 12:15:06 -0300

    Final mobile part

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,31 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
+import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
-// a View é como se fosse a nossa div no html
-import { View, StyleSheet, Image, TextInput } from 'react-native';
+import api from '../services/api';
 
-// há varios tamanhos da imagem logo, mas o próprio React vai chamar o melhor tamanho de acordo com a resolução do dispositivo movel
-import logo from '../assets/logo.png'; // precisa ser .png
+import logo from '../assets/logo.png';
 
-// exportando uma função chamada Login (escolhida ao acaso) que vai retorna o conteudo abaixo
-export default function Login() {
-    return (
-        // estilizando a View - "body"
-        <View style={styles.container}>
-            <Image source={logo} />
+export default function Login({ navigation }) {
+  const [user, setUser] = useState('');
 
-            <TextInput="Digite seu usuário no Github"
-            style={styles.input}
-            />
-        </View>
-    );
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    })
+  }, []);
+
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { user: _id });
+  }
+
+  return (
+    <KeyboardAvoidingView 
+      behavior="padding"
+      enabled={Platform.OS === 'ios'}
+      style={styles.container}
+    >
+      <Image source={logo} />
+
+      <TextInput 
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário no Github"
+        placeholderTextColor="#999"
+        style={styles.input}
+        value={user}
+        onChangeText={setUser}
+      />
+
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
 }
-// estilizando o conteudo do container
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#f5f5f5',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 30
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30
+  },
+
+  input: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
 });
